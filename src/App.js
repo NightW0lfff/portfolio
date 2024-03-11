af85c3cefb460a49a9ccc2a85408d371a9733d0b
NightW0lfff <88699008+NightW0lfff@users.noreply.github.com>
2024-03-12 00:09:23 +1100

    Added logo, title and introduction section

--- src/App.js
+++ src/App.js
@@ -2,9 +2,21 @@ import "./App.css";
 
 function App() {
   return (
-    <div>
-      <h1>Software Engineer</h1>
-      <img src="./resources/icons/code-book.png" alt="coding book" />
+    <div className="main">
+      <div className="title-box">
+        <img
+          className="logo"
+          src={require("./resources/icons/code-book.png")}
+          alt="coding book"
+        />
+        <h2 className="title">Software Engineer</h2>
+      </div>
+
+      <div className="content">
+        <h1>Hi there,</h1>
+        <h1>I'm Long Nguyen</h1>
+        <h3>| Software Engineer |</h3>
+      </div>
     </div>
   );
 }
