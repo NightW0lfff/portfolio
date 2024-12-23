b59c1d2ef6f7aa84a51e1e46d9afb9287d643d47
NightW0lfff <philong170101@gmail.com>
2024-12-24 09:51:32 +1100

    Fixing the escape single quote

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
           <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">Software Engineer</span>
             <h1 className="h1">
-              Hello I'm <br />
+              Hello I&apos;m <br />
               <span className="text-accent">Long Nguyen</span>
             </h1>
             <p className="max-w-[500px] mb-9 text-white/80">
